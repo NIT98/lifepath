d9c81125a1c6a36fc0875c16c92b20bf0c604b0d
SajjadSeifi <s.09334497255@gmail.com>
2022-02-26 19:00:52 -0800

    build : base ast node of grammar

--- astlp.py
+++ astlp.py
@@ -7,3 +7,14 @@ class Node( object ):
 
     def first(self):
         return self.children[0]
+
+
+class ASTLp( Node ):
+    def __init__(self):
+        self.label = "lp"
+        super().__init__()
+
+class ASTStmt ( Node ):
+    def __init__(self):
+        self.label = "stmt"
+        super().__init__()
