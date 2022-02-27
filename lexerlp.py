79f05ab9d71c46071ffdab02a2ffb0bbe2985a7a
SajjadSeifi <s.09334497255@gmail.com>
2022-02-26 16:53:17 -0800

    build : drop token function

--- lexerlp.py
+++ lexerlp.py
@@ -1,6 +1,24 @@
 from io import TextIOWrapper as FILE
 
+from iolp import nextch, seekprv
+
 def init(file : str) -> FILE:
     fp : FILE = open(file)
     return fp
 
+def droptoken(fp : FILE) -> str:
+    c =  nextch(fp)
+    seekprv(fp)
+
+    if c == '':
+        return None
+    if c == '-':
+        #impliment comment functionality
+        return droptoken(c)
+    if c.isalpha():
+        return c
+    if c.isnumeric():
+        return c
+
+    print("Lexical Error : near token '%s'" % c)
+    exit(1)
