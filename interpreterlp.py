5da2415835f1f3eae794ac755033ac93ec17dfcc
SajjadSeifi <s.09334497255@gmail.com>
2022-02-26 19:29:43 -0800

    build : interpreter executor

---
+++ interpreterlp.py
@@ -0,0 +1,10 @@
+import astlp as AST
+from stacklp import StackLP
+
+stk = StackLP(1024)
+
+def interpreter( ast : AST.Node ):
+    if isinstance(ast,AST.ASTLp):
+        pass
+    else:
+        print("ast error: please entered root ast LP");
