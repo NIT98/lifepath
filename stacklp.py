532927635c327a461d815fb5329fda6b6f068d80
SajjadSeifi <s.09334497255@gmail.com>
2022-02-26 19:23:02 -0800

    build : push method of stack

--- stacklp.py
+++ stacklp.py
@@ -2,4 +2,12 @@ class StackLP:
     def __init__(self,size:int) -> None:
         self.size = size
         self.board = [0] * size
-        self.curidx = -1 
+        self.curidx = -1
+    
+    def push(self,num):
+        if self.curidx + 1 >= len(self.board):
+            print("overflow stack!")
+            exit(1)
+
+        self.curidx += 1
+        self.board[self.curidx] = int(num)
